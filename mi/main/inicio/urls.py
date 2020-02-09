94ad8b7e86a45a5d406c9d46f7f2e618313d2d2f
RodrigoGarridoLeiva <rodrigogarridoleiva@gmail.com>
2020-02-09 19:03:45 -0300

    reset

--- mi/main/inicio/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url,re_path
-from django.urls import path, include
-from inicio import views
-
-urlpatterns =[
-url(r'^$', views.HomeView,name="main"),
-]
--- mi/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\rodrigo\desktop\sv\appsv\mi\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
